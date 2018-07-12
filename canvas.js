5f6bd8624942f70097ab1ea4393cda1a028aaa6f
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2018-07-12 12:26:45 +0100

    Use points and vectors to do collision maths more thoroughly

--- canvas.js
+++ canvas.js
@@ -2,19 +2,76 @@ var canvas;
 var ctx;
 var balls = [];
 
-var Ball = function(x,y,radius,color) {
+var Point = function(x,y) {
   this.x = x;
   this.y = y;
+};
+Point.prototype.vectorTo = function(point) {
+  return new Vector(point.x - this.x, point.y - this.y);
+};
+Point.prototype.displacedBy = function(vector) {
+  return new Point(this.x + vector.x, this.y + vector.y);
+};
+Point.prototype.displace = function(vector) {
+  this.x += vector.x;
+  this.y += vector.y;
+  return this;
+};
+
+var Vector = function(x,y) {
+  Point.call(this, x, y);
+}
+Vector.prototype = Object.create(Point.prototype);
+Vector.prototype.constructor = Vector;
+Vector.prototype.vectorTo = undefined;
+Vector.prototype.displacedBy = undefined;
+Vector.prototype.displace = undefined;
+Vector.prototype.add = function(vector) {
+  return new Vector(this.x + vector.x, this.y + vector.y);
+};
+Vector.prototype.subtract = function(vector) {
+  return new Vector(this.x - vector.x, this.y - vector.y);
+};
+Vector.prototype.magnitude = function() {
+  return Math.sqrt(this.x * this.x + this.y * this.y);
+};
+Vector.prototype.unit = function() {
+  var mag = this.magnitude();
+  return new Vector(this.x / mag, this.y / mag);
+};
+Vector.prototype.scale = function(scale) {
+  this.x *= scale;
+  this.y *= scale;
+  return this;
+};
+Vector.prototype.scaledBy = function(scale) {
+  return new Vector(this.x * scale, this.y * scale);
+};
+Vector.prototype.dotProduct = function(vector) {
+  return this.x * vector.x + this.y * vector.y;
+}
+Vector.prototype.reversed = function() {
+  return new Vector(-this.x, -this.y);
+};
+
+
+
+var Ball = function(x,y,radius,color) {
+  Point.call(this, x, y);
   this.radius = radius;
   this.color = color;
 
   // Randomise velocities
-  this.vx = (Math.random() * 2) -1;
-  this.vy = (Math.random() * 2) -1;
+  this.velocity = new Vector(
+    (Math.random() * 2) -1,
+    (Math.random() * 2) -1
+  );
 
   balls.push(this);
 
 };
+Ball.prototype = Object.create(Point.prototype);
+Ball.prototype.constructor = Ball;
 Ball.prototype.draw = function() {
   ctx.fillStyle = this.color;
   ctx.beginPath();
@@ -23,15 +80,14 @@ Ball.prototype.draw = function() {
   ctx.fill();
 };
 Ball.prototype.move = function() {
-  this.x += this.vx;
-  this.y += this.vy;
+  this.displace(this.velocity);
 
   // Check for collision with edge
   if (this.x <= this.radius || this.x >= canvas.width - this.radius) {
-    this.vx = - this.vx;
+    this.velocity.x = - this.velocity.x;
   }
   if (this.y <= this.radius || this.y >= canvas.height - this.radius) {
-    this.vy = - this.vy;
+    this.velocity.y = - this.velocity.y;
   }
 
   // Check for collisions with other balls
@@ -40,6 +96,7 @@ Ball.prototype.move = function() {
       continue;
     }
     var collide = doCollision(this, balls[i]);
+    var collide = false;
     if (collide) {
       // Don't do this collision again when processing the other ball
       balls[i].collisions.push(this);
@@ -48,17 +105,9 @@ Ball.prototype.move = function() {
 };
 Ball.prototype.capVelocity = function() {
   var max = 5;
-  if (this.vx > max) {
-    this.vx = max;
-  }
-  if (this.vx < -max) {
-    this.vx = -max;
-  }
-  if (this.vy > max) {
-    this.vy = max;
-  }
-  if (this.vy < -max) {
-    this.vy = -max;
+  var mag = this.velocity.magnitude();
+  if (mag > max) {
+    this.velocity.scale(max/mag);
   }
   // Move away from edges
   if (this.x < this.radius) {
@@ -82,44 +131,35 @@ Ball.resetCollisions = function() {
 
 var doCollision = function(ball1, ball2) {
   var dist = ball1.radius + ball2.radius;
-  var dx = Math.abs(ball1.x - ball2.x);
-  if (dx > dist) {
+  var between = ball1.vectorTo(ball2);
+  if (between.x > dist || between.y > dist) {
     // Collision not possible
     return false;
   }
-  var dy = Math.abs(ball1.y - ball2.y);
-  if (dy > dist) {
-    // Collision not possible
-    return false;
-  }
-  var between = Math.sqrt(dx*dx + dy*dy);
-  var collide = between <= dist;
+  var distBetween = between.magnitude();
+  var collide = distBetween <= dist;
   if (collide) {
 
-    var scalex = (ball2.x - ball1.x) / between;
-    var scaley = (ball2.y - ball1.y) / between;
+    // Get the closing velocity of ball1 on ball2
+    var closingVelocity = ball1.velocity.subtract(ball2.velocity);
 
-    // Do collision as if ball 2 was stationary
+    // Project this along directon of contact
+    var contactDir = between.unit();
+    var scale = closingVelocity.dotProduct(contactDir);
+    var transfer = contactDir.scaledBy(scale);
 
-    // Calculate the velocity transfers
-    var tx = (ball1.vx - ball2.vx) * scalex;
-    var ty = (ball1.vy - ball2.vy) * scaley;
-    //var tx2 = ball2.vx * scalex;
-    //var ty2 = ball2.vy * scaley;
-    // Set new velocities
-    ball1.vx -= tx;
-    ball1.vy -= ty;
-    ball2.vx += tx;
-    ball2.vy += ty;
+    // Transfer this velocity from ball1 to ball2
+    ball1.velocity = ball1.velocity.subtract(transfer);
+    ball2.velocity = ball2.velocity.add(transfer);
+
     // Cap max velocities
     ball1.capVelocity();
     ball2.capVelocity();
     // Move appart
-    var overlap = dist - between;
-    ball2.x += (overlap / 2) * scalex;
-    ball2.y += (overlap / 2) * scaley;
-    ball1.x -= (overlap / 2) * scalex;
-    ball1.y -= (overlap / 2) * scaley;
+    var overlap = dist - distBetween;
+    var dispacement = contactDir.scaledBy(overlap/2);
+    ball1.displace(dispacement.reversed());
+    ball2.displace(dispacement);
   }
   return collide;
 };
