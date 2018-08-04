7c44f5bd3d6d1b5359a0d78f7eed046d4d95bfde
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2018-08-04 19:31:53 +0100

    Tests for complexbox

--- test/complexbox.test.js
+++ test/complexbox.test.js
@@ -7,7 +7,7 @@ chai.use(chaiAlmost());
 import { Complex } from '../lib/complex';
 import { ComplexBox } from '../lib/complexbox';
 
-describe.only('ComplexBox', function() {
+describe('ComplexBox', function() {
   it('constructor', function() {
     const c1 = new Complex(-1,2);
     const c2 = new Complex(2,-1);
@@ -31,5 +31,23 @@ describe.only('ComplexBox', function() {
     const box = new ComplexBox(c1,c2);
     expect(box.midPoint()).to.deep.equal(box.pointAt(0.5,0.5));
     expect(box.midPoint()).to.deep.equal({r:0.5,i:-0.5});
+  });
+
+  it('isOutside', function() {
+    const c1 = new Complex(-2,-2);
+    const c2 = new Complex(2,2);
+    const box1 = new ComplexBox(c1,c2);
+    expect(new ComplexBox({r:-4,i:-2},{r:-2.5,i:2}).isOutside(box1)).to.be.true;
+    expect(new ComplexBox({r:4,i:-2},{r:6,i:2}).isOutside(box1)).to.be.true;
+    expect(new ComplexBox({r:-2,i:-4},{r:2,i:-3.5}).isOutside(box1)).to.be.true;
+    expect(new ComplexBox({r:-2,i:2.5},{r:2,i:4}).isOutside(box1)).to.be.true;
+    expect(new ComplexBox({r:-1,i:-1},{r:1,i:1}).isOutside(box1)).to.be.false;
+    expect(new ComplexBox({r:-2,i:-1},{r:1,i:1}).isOutside(box1)).to.be.false;
+    // Touching
+    expect(new ComplexBox({r:-3,i:-2},{r:-2,i:2}).isOutside(box1)).to.be.true;
+    // Touching within tol
+    expect(new ComplexBox({r:-3,i:-2},{r:-2 + 1e-8,i:2}).isOutside(box1)).to.be.true;
+    // Overlap by more than tol
+    expect(new ComplexBox({r:-3,i:-2},{r:-2 + 1e-3,i:2}).isOutside(box1)).to.be.false;
   });
 });
